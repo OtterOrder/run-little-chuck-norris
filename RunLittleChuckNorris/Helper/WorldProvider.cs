8289db7121a9fd4d638acd34551de9512422a319
florian.brulhart@gmail.com <florian.brulhart@gmail.com@d2825bb6-d8a6-1334-82ca-06c916739865>
2010-04-27 10:04:06 +0000

    - create and implement WorldProvider

---
+++ RunLittleChuckNorris/Helper/WorldProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RunLittleChuckNorris.Helper
+{
+    public interface IWorldProvider
+    {
+        #region Properties
+
+        List<GameObject.Obstacle> Obstacles { get; }
+        List<GameObject.Plateforme> Plateformes { get; }
+
+        #endregion
+    }
+}
