5328abb610def63d3a4f50e67253046c8842804b
florian.brulhart@gmail.com <florian.brulhart@gmail.com@d2825bb6-d8a6-1334-82ca-06c916739865>
2010-04-27 09:22:55 +0000

    add IGameOver

---
+++ trunk/RunLittleChuckNorris/Helper/IGameOver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RunLittleChuckNorris.Helper
+{
+    public interface IGameOver
+    {
+        void GameOver();
+    }
+}
